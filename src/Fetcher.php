c68c81eb8a9f4c36dfe341d6d36c619f4078f772
Niek van der Velde <niek@velde.xyz>
2022-09-20 12:00:53 +0200

    Implement media fetcher

--- src/Fetcher.php
+++ src/Fetcher.php
@@ -47,6 +47,14 @@ class Fetcher
             return;
         }
 
+        if (isset($post->media)) {
+            $result = $this->fetchMedia($post->media, $platform, $post->id);
+
+            if ($result !== false) {
+                $post->media = $result['path'];
+            }
+        }
+
         $wpdb->get_var(
             $wpdb->prepare(
                 "INSERT INTO {$tableName} (platform, post_id, post_data, post_date, fetched_at) VALUES (%s, %s, %s, %s, %s)",
@@ -57,5 +65,66 @@ class Fetcher
                 date('Y-m-d H:i:s')
             )
         );
+    }
+
+    /**
+     * Fetch media.
+     *
+     * @param string $imageUrl
+     *
+     * @return void
+     *
+     * @author Niek van der Velde <niek@aimtofeel.com>
+     * @version 1.0.0
+     */
+    public function fetchMedia(string $mediaUrl, string $platform, string $postId)
+    {
+        require_once ABSPATH . 'wp-admin/includes/file.php';
+
+        $tempFile = download_url($mediaUrl);
+
+        if (is_wp_error($tempFile)) {
+            return false;
+        }
+
+        $file = [
+            'name' => "{$platform}-{$postId}." . explode('.', explode('?', basename($mediaUrl))[0])[1],
+            'type' => mime_content_type($tempFile),
+            'tmp_name' => $tempFile,
+            'size' => filesize($tempFile),
+        ];
+
+        $sideLoad = wp_handle_sideload(
+            $file,
+            ['test_form' => false]
+        );
+
+        if (!empty($sideLoad['error'])) {
+            return false;
+        }
+
+        $attachmentId = wp_insert_attachment(
+            [
+                'guid' => $sideLoad['url'],
+                'post_mime_type' => $sideLoad['type'],
+                'post_title' => basename($sideLoad['file']),
+                'post_content' => '',
+                'post_status' => 'inherit',
+            ],
+            $sideLoad['file']
+        );
+
+        if (is_wp_error($attachmentId) || !$attachmentId) {
+            return false;
+        }
+
+        require_once ABSPATH . 'wp-admin/includes/image.php';
+
+        wp_update_attachment_metadata(
+            $attachmentId,
+            wp_generate_attachment_metadata($attachmentId, $sideLoad['file'])
+        );
+
+        return parse_url(wp_get_attachment_url($attachmentId));
     }
 }
