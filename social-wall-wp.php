93847b7dc32a643454ed004f03e29d1806d88d10
Niek van der Velde <niek@velde.xyz>
2022-09-23 11:11:09 +0200

    Change version to 1.4.3

--- social-wall-wp.php
+++ social-wall-wp.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Social Wall WP
  * Description: Collects social media activity and stores them in a shared table.
- * Version: 1.4.2
+ * Version: 1.4.3
  * Author: AimToFeel
  * Author URI: https://aimtofeel.com
  * License: GPLv2
