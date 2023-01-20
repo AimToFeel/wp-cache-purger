7ef6e3e26900a82dcd0d09afe9ee07845ba73730
Niek van der Velde <niek@velde.xyz>
2023-01-20 15:15:24 +0100

    v1.6.1

--- cache-purger.php
+++ cache-purger.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Cache purger
  * Description: Purges server side cache by calling a purger api
- * Version: 1.0.0
+ * Version: 1.6.1
  * Author: AimToFeel
  * Author URI: https://aimtofeel.com
  * License: GPLv2
