97ed8fc1ff1d7972a8abd0f57aa7bf14642d765c
Niek van der Velde <niek@velde.xyz>
2023-04-26 09:47:48 +0200

    Add clear single page cache to admin tollbar

--- src/WpCachePurger.php
+++ src/WpCachePurger.php
@@ -55,21 +55,180 @@ class WpCachePurger
 
     public function defineAdminClearCacheButton($wp_admin_bar): void
     {
-        if (!$this->isAdminPanel) {
-            return;
-        }
+        echo '
+        <style type="text/css">
+            .wp-cache-purger-admin-bar-button {
+                display: block !important;
+            }
 
-        $args = [
-            'id' => 'wp-cache-purger',
-            'parent' => null,
-            'group' => null,
-            'title' => '<span class="ab-icon"></span> Clear cache',
-            'meta' => [
-                'class' => 'wp-cache-purger-admin-bar-button',
-                'title' => 'Clear the whole site cache',
-                'onclick' => 'wpCachePurgerPurgerOpenModal();',
-            ],
-        ];
+            .wp-cache-purger-admin-bar-button div:hover {
+                cursor: pointer !important;
+                color: #d63638 !important;
+            }
+
+            .wp-cache-purger-admin-bar-button .ab-icon::before {
+                content: "\f17c";
+                top: 3px;
+            }
+
+            .wp-cache-purger-admin-bar-button div:hover .ab-icon::before {
+                color: #d63638 !important;
+            }
+
+            .wp-cache-purger-site-bar-button {
+                display: block !important;
+            }
+
+            .wp-cache-purger-site-bar-button div:hover {
+                cursor: pointer !important;
+                color: #d63638 !important;
+            }
+
+            .wp-cache-purger-site-bar-button .ab-icon::before {
+                content: "\f17c";
+                top: 3px;
+            }
+
+            .wp-cache-purger-site-bar-button div:hover .ab-icon::before {
+                color: #d63638 !important;
+            }
+
+            @media screen and (max-width: 782px) {
+                .wp-cache-purger-site-bar-button, .wp-cache-purger-admin-bar-button {
+                    width: 52px !important;
+                    overflow: hidden !important;
+                }
+            }
+
+            .wp-cache-purger-modal {
+                top: 0;
+                left: 0;
+                position: fixed;
+                height: 100%;
+                width: 100%;
+                z-index: 100;
+                display: none;
+            }
+
+            .wp-cache-purger-modal--open {
+                display: block;
+            }
+
+            .wp-cache-purger-modal__inner {
+                position: absolute;
+                top: 20%;
+                left: 50%;
+                width: calc(100vw - 48px);
+                max-width: 400px;
+                transform: translateX(-50%);
+                background: #fff;
+                z-index: 100;
+                padding: 24px;
+            }
+
+            .wp-cache-purger-modal__backdrop {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100vw;
+                height: 100vh;
+                z-index: 99;
+                background: #000;
+                opacity: 0.6;
+            }
+
+            .wp-cache-purger-modal__button-container {
+                display: flex;
+                flex-direction: row;
+                gap: 24px;
+            }
+
+            .wp-cache-purger-modal__button {
+                background: #fff;
+                border-radius: 3px;
+                border: 1px solid #2271b1;
+                flex: 1;
+                text-align: center;
+                color: #2271b1;
+                line-height: 40px;
+                font-weight: bold;
+            }
+
+            .wp-cache-purger-modal__button--confirm {
+                border: 1px solid #d63638;
+                color: #d63638;
+            }
+
+            .wp-cache-purger-modal__button--confirm:hover {
+                cursor: pointer;
+                background: #f6d6d7;
+            }
+
+            .wp-cache-purger-modal__button--cancel {
+                border: 1px solid #2271b1;
+                color: #2271b1;
+            }
+
+            .wp-cache-purger-modal__button--cancel:hover {
+                cursor: pointer;
+                background: #cce3f5;
+            }
+
+            .wp-cache-purger-modal__title {
+                margin: 0 0 8px 0;
+            }
+
+            .wp-cache-purger-modal__text {
+                margin: 0 0 24px 0;
+            }
+        </style>
+        ';
+
+        $args = [];
+
+        if (!$this->isAdminPanel) {
+            echo '
+                <script>
+                    async function wpCachePurgerPurgePage() {
+                        await fetch("/cache-purger/purge", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                                "Accept": "application/json"
+                            },
+                            body: JSON.stringify({
+                                url: "' . (isset($_SERVER['HTTPS']) ? 'https' : 'http') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" . '",
+                                authenticationToken: "' . get_option('wp_cache_purger_authentication_token') . '"
+                            })
+                        });
+                    }
+                </script>
+            ';
+
+            $args = [
+                'id' => 'wp-cache-purger',
+                'parent' => null,
+                'group' => null,
+                'title' => '<span class="ab-icon"></span> Clear cache',
+                'meta' => [
+                    'class' => 'wp-cache-purger-site-bar-button',
+                    'title' => 'Clear current page cache',
+                    'onclick' => 'wpCachePurgerPurgePage();',
+                ],
+            ];
+        } else {
+            $args = [
+                'id' => 'wp-cache-purger',
+                'parent' => null,
+                'group' => null,
+                'title' => '<span class="ab-icon"></span> Clear cache',
+                'meta' => [
+                    'class' => 'wp-cache-purger-admin-bar-button',
+                    'title' => 'Clear the whole site cache',
+                    'onclick' => 'wpCachePurgerPurgerOpenModal();',
+                ],
+            ];
+        }
 
         $wp_admin_bar->add_node($args);
     }
@@ -79,104 +238,6 @@ class WpCachePurger
         $this->isAdminPanel = true;
 
         echo '
-            <style type="text/css">
-                .wp-cache-purger-admin-bar-button div:hover {
-                    cursor: pointer !important;
-                    color: #d63638 !important;
-                }
-
-                .wp-cache-purger-admin-bar-button .ab-icon::before {
-                    content: "\f17c";
-                    top: 3px;
-                }
-
-                .wp-cache-purger-admin-bar-button div:hover .ab-icon::before {
-                    color: #d63638 !important;
-                }
-
-                .wp-cache-purger-modal {
-                    top: 0;
-                    left: 0;
-                    position: fixed;
-                    height: 100%;
-                    width: 100%;
-                    z-index: 100;
-                    display: none;
-                }
-
-                .wp-cache-purger-modal--open {
-                    display: block;
-                }
-
-                .wp-cache-purger-modal__inner {
-                    position: absolute;
-                    top: 20%;
-                    left: 50%;
-                    width: calc(100vw - 48px);
-                    max-width: 400px;
-                    transform: translateX(-50%);
-                    background: #fff;
-                    z-index: 100;
-                    padding: 24px;
-                }
-
-                .wp-cache-purger-modal__backdrop {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100vw;
-                    height: 100vh;
-                    z-index: 99;
-                    background: #000;
-                    opacity: 0.6;
-                }
-
-                .wp-cache-purger-modal__button-container {
-                    display: flex;
-                    flex-direction: row;
-                    gap: 24px;
-                }
-
-                .wp-cache-purger-modal__button {
-                    background: #fff;
-                    border-radius: 3px;
-                    border: 1px solid #2271b1;
-                    flex: 1;
-                    text-align: center;
-                    color: #2271b1;
-                    line-height: 40px;
-                    font-weight: bold;
-                }
-
-                .wp-cache-purger-modal__button--confirm {
-                    border: 1px solid #d63638;
-                    color: #d63638;
-                }
-
-                .wp-cache-purger-modal__button--confirm:hover {
-                    cursor: pointer;
-                    background: #f6d6d7;
-                }
-
-                .wp-cache-purger-modal__button--cancel {
-                    border: 1px solid #2271b1;
-                    color: #2271b1;
-                }
-
-                .wp-cache-purger-modal__button--cancel:hover {
-                    cursor: pointer;
-                    background: #cce3f5;
-                }
-
-                .wp-cache-purger-modal__title {
-                    margin: 0 0 8px 0;
-                }
-
-                .wp-cache-purger-modal__text {
-                    margin: 0 0 24px 0;
-                }
-            </style>
-
             <script>
                 function wpCachePurgerPurgerOpenModal() {
                     const modal = document.getElementById("wp-cache-purger-modal");
